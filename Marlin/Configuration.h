16bbdcc723359d694a78938b3a0d18e430b535b9
Mike Baran <mbaran@dignitastechnologies.com>
2020-04-20 01:26:53 -0400

    PID Tune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -372,9 +372,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 20.55
+  #define DEFAULT_Ki 1.77
+  #define DEFAULT_Kd 59.76
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
